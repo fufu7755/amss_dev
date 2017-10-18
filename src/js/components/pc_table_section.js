d7982d95537b7c81ab7176730da4890ac206a0c9
fufu7755 <fufu7755@gmail.com>
2017-10-18 23:14:36 +0800

    10-18

--- src/js/components/pc_table_section.js
+++ src/js/components/pc_table_section.js
@@ -9,145 +9,149 @@ export default class PCTableSection extends React.Component {
       <div className="tableSection">
         <div className="container">
           <Row>
-            <Col className="tableTop" span={4}>
-              <h4>AMSS-1</h4>
-              <p>投资回报测算+估值</p>
-            </Col>
-            <Col className="tableTop" span={4}>
-              <h4>AMSS-2</h4>
-              <p>招商管理+回报再测算</p>
-            </Col>
-            <Col className="tableTop" span={4}>
-              <h4>AMSS-3</h4>
-              <p>经营预算管理+回报再测算</p>
-            </Col>
-            <Col className="tableTop" span={4}>
-              <h4>AMSS-4</h4>
-              <p>投资审批+风险控制</p>
-            </Col>
-            <Col className="tableTop" span={4}>
-              <h4>AMSS-5</h4>
-              <p></p>
-            </Col>
-            <Col className="tableTop" span={4}>
-              <h4>AMSS-6</h4>
-              <p></p>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={4}>
-              <img src="/src/images/shop.png" alt=""/>
-              <p>购物中心</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/shop.png" alt=""/>
-              <p>购物中心</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/shop.png" alt=""/>
-              <p>购物中心</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/shop.png" alt=""/>
-              <p>购物中心</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/shop.png" alt=""/>
-              <p>购物中心</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/shop.png" alt=""/>
-              <p>购物中心</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={4}>
-              <img src="/src/images/building.png" alt=""/>
-              <p>写字楼</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/building.png" alt=""/>
-              <p>写字楼</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/building.png" alt=""/>
-              <p>写字楼</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/building.png" alt=""/>
-              <p>写字楼</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/building.png" alt=""/>
-              <p>写字楼</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/building.png" alt=""/>
-              <p>写字楼</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={4}>
-              <img src="/src/images/hotel.png" alt=""/>
-              <p>酒店公寓</p>
-            </Col>
-            <Col span={4}>
-            </Col>
-            <Col span={4}>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/hotel.png" alt=""/>
-              <p>酒店公寓</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/hotel.png" alt=""/>
-              <p>酒店公寓</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/hotel.png" alt=""/>
-              <p>酒店公寓</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={4}>
-              <img src="/src/images/cash.png" alt=""/>
-              <p>物业销售</p>
-            </Col>
-            <Col span={4}>
-            </Col>
-            <Col span={4}>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/cash.png" alt=""/>
-              <p>物业销售</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/cash.png" alt=""/>
-              <p>物业销售</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/cash.png" alt=""/>
-              <p>物业销售</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={4}>
-              <img src="/src/images/all.png" alt=""/>
-              <p>综合体</p>
-            </Col>
-            <Col span={4}>
-            </Col>
-            <Col span={4}>
-            </Col>
-            <Col span={4}>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/all.png" alt=""/>
-              <p>综合体</p>
-            </Col>
-            <Col span={4}>
-              <img src="/src/images/all.png" alt=""/>
-              <p>综合体</p>
+            <Col span={22} offset={1}>
+              <Row>
+                <Col className="tableTop" span={4}>
+                  <h4>AMSS-1</h4>
+                  <p>投资回报测算+估值</p>
+                </Col>
+                <Col className="tableTop" span={4}>
+                  <h4>AMSS-2</h4>
+                  <p>招商管理+回报再测算</p>
+                </Col>
+                <Col className="tableTop" span={4}>
+                  <h4>AMSS-3</h4>
+                  <p>经营预算管理+回报再测算</p>
+                </Col>
+                <Col className="tableTop" span={4}>
+                  <h4>AMSS-4</h4>
+                  <p>投资审批+风险控制</p>
+                </Col>
+                <Col className="tableTop" span={4}>
+                  <h4>AMSS-5</h4>
+                  <p></p>
+                </Col>
+                <Col className="tableTop" span={4}>
+                  <h4>AMSS-6</h4>
+                  <p></p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={4}>
+                  <img src="/src/images/shop.png" alt=""/>
+                  <p>购物中心</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/shop.png" alt=""/>
+                  <p>购物中心</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/shop.png" alt=""/>
+                  <p>购物中心</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/shop.png" alt=""/>
+                  <p>购物中心</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/shop.png" alt=""/>
+                  <p>购物中心</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/shop.png" alt=""/>
+                  <p>购物中心</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={4}>
+                  <img src="/src/images/building.png" alt=""/>
+                  <p>写字楼</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/building.png" alt=""/>
+                  <p>写字楼</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/building.png" alt=""/>
+                  <p>写字楼</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/building.png" alt=""/>
+                  <p>写字楼</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/building.png" alt=""/>
+                  <p>写字楼</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/building.png" alt=""/>
+                  <p>写字楼</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={4}>
+                  <img src="/src/images/hotel.png" alt=""/>
+                  <p>酒店公寓</p>
+                </Col>
+                <Col span={4}>
+                </Col>
+                <Col span={4}>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/hotel.png" alt=""/>
+                  <p>酒店公寓</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/hotel.png" alt=""/>
+                  <p>酒店公寓</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/hotel.png" alt=""/>
+                  <p>酒店公寓</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={4}>
+                  <img src="/src/images/cash.png" alt=""/>
+                  <p>物业销售</p>
+                </Col>
+                <Col span={4}>
+                </Col>
+                <Col span={4}>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/cash.png" alt=""/>
+                  <p>物业销售</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/cash.png" alt=""/>
+                  <p>物业销售</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/cash.png" alt=""/>
+                  <p>物业销售</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={4}>
+                  <img src="/src/images/all.png" alt=""/>
+                  <p>综合体</p>
+                </Col>
+                <Col span={4}>
+                </Col>
+                <Col span={4}>
+                </Col>
+                <Col span={4}>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/all.png" alt=""/>
+                  <p>综合体</p>
+                </Col>
+                <Col span={4}>
+                  <img src="/src/images/all.png" alt=""/>
+                  <p>综合体</p>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </div>
--- src/js/components/pc_top_section.js
+++ src/js/components/pc_top_section.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Row, Col} from 'antd'
 
 export default class PCTopSection extends React.Component {
 
@@ -6,53 +7,58 @@ export default class PCTopSection extends React.Component {
     return (
       <div className="topSection">
         <div className="container">
-          <ul className="topNav">
-            <li>投资</li>
-            <li>设计</li>
-            <li>建设</li>
-            <li>招商</li>
-            <li>运营</li>
-            <li>退出</li>
-          </ul>
+          <Row>
+            <Col span={20} offset={2}>
+              <ul className="topNav">
+                <li>投资</li>
+                <li>设计</li>
+                <li>建设</li>
+                <li>招商</li>
+                <li>运营</li>
+                <li>退出</li>
+              </ul>
 
-          <div className="row">
-            <div className="col-4">
-              <div className="icon-item amss1-item">
-                <img src="/src/images/icons/amss1-item.png" alt=""/>
+              <div className="row">
+                <div className="col-4">
+                  <div className="icon-item amss1-item">
+                    <img src="/src/images/icons/amss1-item.png" alt=""/>
+                  </div>
+                  <div className="icon-item">
+                    <img src="/src/images/icons/amss4-item.png" alt=""/>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="icon-item amss1-item">
+                    <img src="/src/images/icons/amss1-item.png" alt=""/>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="icon-item amss1-item">
+                    <img src="/src/images/icons/amss1-item.png" alt=""/>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="icon-item">
+                    <img src="/src/images/icons/amss2-item.png" alt=""/>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="icon-item">
+                    <img src="/src/images/icons/amss3-item.png" alt=""/>
+                  </div>
+                  <div className="icon-item">
+                    <img src="/src/images/icons/amss5-item.png" alt=""/>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <div className="icon-item">
+                    <img src="/src/images/icons/amss6-item.png" alt=""/>
+                  </div>
+                </div>
               </div>
-              <div className="icon-item">
-                <img src="/src/images/icons/amss4-item.png" alt=""/>
-              </div>
-            </div>
-            <div className="col-4">
-              <div className="icon-item amss1-item">
-                <img src="/src/images/icons/amss1-item.png" alt=""/>
-              </div>
-            </div>
-            <div className="col-4">
-              <div className="icon-item amss1-item">
-                <img src="/src/images/icons/amss1-item.png" alt=""/>
-              </div>
-            </div>
-            <div className="col-4">
-              <div className="icon-item">
-                <img src="/src/images/icons/amss2-item.png" alt=""/>
-              </div>
-            </div>
-            <div className="col-4">
-              <div className="icon-item">
-                <img src="/src/images/icons/amss3-item.png" alt=""/>
-              </div>
-              <div className="icon-item">
-                <img src="/src/images/icons/amss5-item.png" alt=""/>
-              </div>
-            </div>
-            <div className="col-4">
-              <div className="icon-item">
-                <img src="/src/images/icons/amss6-item.png" alt=""/>
-              </div>
-            </div>
-          </div>
+
+            </Col>
+          </Row>
 
         </div>
 
